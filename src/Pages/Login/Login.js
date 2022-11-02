41b9e1281d028ec295bf631bec3b7cc5c037c68c
Ather1230 <ather.biswas0057@gmail.com>
2022-11-03 00:35:36 +0600

    updated

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -39,7 +39,7 @@ const Login = () => {
                     </label>
                     <input name='password' type="password" placeholder="password" className="input input-bordered" required/>
                     <label className="label">
-                        <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                        <Link className="label-text-alt link link-hover"> Forgot password?</Link>
                     </label>
                     </div>
                     <div className="form-control mt-6">
